393cd86974854d7cbc94273260d6cdbb3213ae36
yuvraj.singh <yuvraj.singh@sunarctechnologies.com>
2019-02-04 12:34:40 +0530

    created two component & added css / js

--- src/components/Pos.vue
+++ src/components/Pos.vue
@@ -1,33 +1,44 @@
 <template>
-  <div class="title">
-    <h1>{{ msg }} </h1>
+    <div class="title">
+        <h1>{{ msg }} </h1>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <Header/>
+                <LeftNavCategory/>
+            </div>
+        </nav>
+    </div>
     <Toolbar msg="Broccoli POS Header Toolbar"/>
     <Content msg="Broccoli POS Content"/>
-  </div>
+    </div>
 </template>
 
 <script>
-import Toolbar from './pos/Toolbar.vue'
-import Content from './pos/Content.vue'
+    import LeftNavCategory from './pos/LeftNavCategory.vue'
+    import Header from './pos/Header.vue'
+    import Toolbar from './pos/Toolbar.vue'
+    import Content from './pos/Content.vue'
 
-export default {
-  name: 'Pos',
-  components : {
-    Toolbar,
-    Content
-  },
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'Pos',
+        components: {
+            Header,
+            LeftNavCategory,
+            Toolbar,
+            Content
+        },
+        props: {
+            msg: String
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
 <style lang="scss" scoped>
-@import '@/assets/sass/variables.scss';
+    @import '@/assets/sass/variables.scss';
 
-.title {
-  color : $primary-color;
-}
+    .title {
+        color: $primary-color;
+    }
 </style>
---
+++ src/components/pos/LeftNavCategory.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="LeftNavCategory">
+        <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+            <li class="nav-item logo-wrap" data-toggle="tooltip" data-placement="right" title="" data-original-title="logo">
+                <a class="nav-link" href="">
+                    <!--<img src="{{url('/images/icon.png')}}">-->
+                </a>
+            </li>
+
+            <!--<li class="nav-item active-opacity" data-toggle="tooltip" data-placement="right" title=""
+                data-original-title="Pizza">
+                <a class="nav-link active" href="">
+                    &lt;!&ndash;<img src="{{url('/images/pos/pizza.svg')}}">&ndash;&gt;
+                    <span class="nav-link-text">Pizza</span>
+                </a>
+            </li>-->
+        </ul>
+
+        <ul class="navbar-nav sidenav-toggler">
+            <li class="nav-item arrow-bottom">
+                <a class="nav-link" href="#">down-arrow
+                    <!--<img class="bt-arrow" src="{{url('/images/pos/down-arrow.png')}}">-->
+                    <!--<img class="top-arrow" src="{{url('/images/pos/top-arrow.png')}}">-->
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-center" id="sidenavToggler">profile
+                    <!--<img src="{{url('/images/pos/profile-pic.png')}}">-->
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LeftNavCategory',
+        props: {
+        },
+    };
+</script>
