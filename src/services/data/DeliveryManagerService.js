3f5980fd875514ec06b9069bac760c7f2e45ff4e
Tofeeq <tof33q@gmail.com>
2019-02-18 12:12:30 +0500

    All Apis implemented

---
+++ src/services/data/DeliveryManagerService.js
@@ -0,0 +1,9 @@
+import DataService from '@/services/DataService'
+
+export default {
+  assignDriver(...[locationId, orderId, driverId]) {
+    return DataService.post(
+      `/api/auth/deliveryManager/assign/driver/?location_id=${locationId}&order_id=${orderId}&driver_id=${driverId}`
+    )
+  },
+}
---
+++ src/services/data/MenuListService.js
@@ -0,0 +1,7 @@
+import DataService from '@/services/DataService'
+
+export default {
+  fetchAll(staffId) {
+    return DataService.get(`/api/auth/get/Menu/List/?staff_id=${staffId}`)
+  },
+}
