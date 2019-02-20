287b4e74a8f0195a1dcd49ce2e3f154ad3bc7cf6
yuvraj.singh <yuvraj.singh@sunarctechnologies.com>
2019-02-20 18:02:09 +0530

    fixed breadcrumbs

--- src/components/pos/content/catalog/items/Breadcrumbs.vue
+++ src/components/pos/content/catalog/items/Breadcrumbs.vue
@@ -3,40 +3,43 @@
     <ul class="ullist-category">
       <li>
         <span>Selected Category</span>
-        <p>{{ t(selectedCategory.cat_name).name }}</p>
+        <p>{{ t(selectedCategory.cat_name).name.substring(0, 15) }}</p>
         <span class="arrows"
           ><img src="img/pos/right-arrow.png" alt="right-arrow"
         /></span>
       </li>
-      <li>
+      <li v-if="selectedSubCategory">
         <span>Selected Sub Category</span>
-        <p>{{ t(selectedSubCategory.subcategory_name).name }}</p>
+        <p>
+          {{ t(selectedSubCategory.subcategory_name).name.substring(0, 15) }}
+        </p>
         <span class="arrows"
           ><img src="img/pos/right-arrow.png" alt="right-arrow"
         /></span>
       </li>
-      <li>
+      <li v-if="selectedItem">
         <span>Selected Item</span>
-        <p>{{ selectedItem }}</p>
+        <p>{{ t(selectedItem.item_name).name.substring(0, 15) }}</p>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 
 export default {
-  name: 'Breadcrumbs',
+  name: "Breadcrumbs",
   props: {
-    msg: String,
+    msg: String
   },
   computed: {
     ...mapState({
       selectedCategory: state => state.category.category,
-      selectedSubCategory: state => state.category.subcategory,
-      selectedItem: state => state.category.item,
-    }),
-  },
-}
+      selectedSubCategory: state =>
+        state.category.subcategory ? state.category.subcategory : false,
+      selectedItem: state => (state.order.item ? state.order.item : false)
+    })
+  }
+};
 </script>
--- src/services/data/LocationService.js
+++ src/services/data/LocationService.js
@@ -1,34 +1,34 @@
-import DataService from '@/services/DataService'
+import DataService from "@/services/DataService";
 
 export default {
   ordersCountByStatus(...[locationId, orderStatus]) {
     return DataService.get(
       `/api/auth/deliveryManager/get/order/?location_id=${locationId}&order_status=${orderStatus}`
-    )
+    );
   },
 
   getOrdersByStatus(...[locationId, orderStatus, collected]) {
     return DataService.get(
       `/api/auth/deliveryManager/get/order/detail?location_id=${locationId}&order_status=${orderStatus}&collected=${collected}`
-    )
+    );
   },
 
   getDispatchScreenOrders(locationId) {
     return DataService.get(
       `/api/auth/deliveryManager/dispatch/screen/?location_id=${locationId}`
-    )
+    );
   },
 
   //Get country,city,location and drivers data by location id .
   getLocationData(...[locationId, staff, lastSyncDate, time]) {
     return DataService.get(
       `/api/auth/locationdetails/?location_id=${locationId}&staff=${staff}&last_sync_date=${lastSyncDate}&time=${time}`
-    )
+    );
   },
 
   getGiftCards(...[locationId, lastSyncDate, isCompress]) {
     return DataService.get(
       `/api/auth/get/gift/cards?location_id=${locationId}&last_sync_date=${lastSyncDate}&is_compress=${isCompress}`
-    )
-  },
-}
+    );
+  }
+};
