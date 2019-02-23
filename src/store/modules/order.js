891cf4ea9b8857a310f3387d5a6ecf8091cc39b5
Tofeeq <tof33q@gmail.com>
2019-02-23 12:10:44 +0500

    Adding modifier location based prices to item location based price in cart

--- src/store/modules/order.js
+++ src/store/modules/order.js
@@ -10,18 +10,18 @@ const state = {
 const getters = {
   orderTotal: () => {
     return state.items.reduce((total, item) => {
-      return total + item.item_price * item.quantity
+      return total + item.price * item.quantity
     }, 0)
   },
 
   subTotal: () => {
     return state.items.reduce((total, item) => {
-      return total + item.item_price * item.quantity
+      return total + item.price * item.quantity
     }, 0)
   },
 
   itemPrice: () => item => {
-    return item.quantity * item.item_price
+    return item.quantity * item.price
   },
 
   orderModifiers: () => item => {
@@ -35,8 +35,16 @@ const getters = {
 
 // actions
 const actions = {
-  addToOrder({ commit, state }, item) {
-    //set item price based on location
+  addToOrder({ state, commit, rootState }, item) {
+    //set item price based on location, for modifiers items it is already set in modifer store
+    if (
+      item.get_item_location_price.location_id == rootState.location.location
+    ) {
+      item.price = parseFloat(item.get_item_location_price.menu_location_price)
+    } else {
+      item.price = parseFloat(item.item_price)
+    }
+
     commit(mutation.SET_ITEM, item)
 
     const index = state.items.findIndex(
@@ -69,6 +77,23 @@ const actions = {
       }
     })
     item.modifiers = itemModifiers
+    //add modifier prices to item price
+    let modifierPrice = 0
+
+    rootState.modifier.itemModifiers.forEach(itemMod => {
+      itemMod.modifiers.forEach(mod => {
+        mod.get_modifier_sub_groups.forEach(subgroup => {
+          subgroup.get_modifier_item_list.forEach(submod => {
+            if (item.modifiers.includes(submod._id)) {
+              modifierPrice += parseFloat(submod.price)
+            }
+          })
+        })
+      })
+    })
+
+    item.price = parseFloat(item.price) + modifierPrice
+
     commit(mutation.SET_ITEM, item)
 
     //check if item exists with same signature
