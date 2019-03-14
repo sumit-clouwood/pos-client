d70e8523f750c39f2f69c856919e199c2725cd5b
Tofeeq <tof33q@gmail.com>
2019-03-14 16:17:14 +0500

    Adding waiting and errors

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -1,31 +1,42 @@
 <template>
   <div class="inventory-location-wrapper pt-5 h-100">
-    <div class="mx-auto col-md-3 mt-5">
-      <div class="search-location p-4" v-if="locationIds.length">
-        <p class="location-title">Please select a location:</p>
-        <form class="form-inline search-inline">
-          <div class="inner-addon left-addon">
-            <img
-              class="search-img"
-              src="img/pos/search-icon.png"
-              alt="search"
-            />
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Search location"
-            />
-          </div>
-        </form>
+    <section v-if="errored">
+      <p>
+        We're sorry, we're not able to proceed at the moment, please try back
+        later
+      </p>
+      <p>{{ errored }}</p>
+    </section>
+    <div v-else class="mx-auto col-md-3 mt-5">
+      <div v-if="loading">
+        <ul class="ullist-inventory-location pl-0 pt-2">
+          <li class="p-3">
+            <a v-if="loaded && locationName">{{ locationName }}</a>
+            <span v-if="!loaded">
+              Loading data...
+            </span>
+          </li>
+        </ul>
       </div>
-      <ul class="ullist-inventory-location pl-0 pt-2">
-        <li class="p-3">
-          <a v-if="loaded && locationName">{{ locationName }}</a>
-          <span v-if="!loaded">
-            Loading data...
-          </span>
-        </li>
-      </ul>
+      <div v-else class="search-location p-4">
+        <div v-if="locationIds.length">
+          <p class="location-title">Please select a location:</p>
+          <form class="form-inline search-inline">
+            <div class="inner-addon left-addon">
+              <img
+                class="search-img"
+                src="img/pos/search-icon.png"
+                alt="search"
+              />
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Search location"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -36,6 +47,13 @@ import Cookie from '@/mixins/Cookie'
 export default {
   name: 'Location',
   props: {},
+  data: function() {
+    return {
+      info: null,
+      loading: true,
+      errored: false,
+    }
+  },
   mixins: [Cookie],
   //life cycle hooks
   beforeCreate() {
@@ -55,26 +73,26 @@ export default {
           this.$store.dispatch('location/setLocations', response.data.data)
         }
 
-        Promise.all([this.$store.dispatch('category/fetchAll', response)]).then(
-          () => {
+        Promise.all([this.$store.dispatch('category/fetchAll', response)])
+          .then(() => {
+            this.$store.dispatch('modifier/fetchAll', response)
             this.$store.commit('sync/loaded', true)
-          }
-        )
-
-        this.$store.dispatch('modifier/fetchAll', response)
-        this.$store.dispatch('location/fetchAll', response)
-        this.$store.dispatch('announcement/fetchAll', response)
-        this.$store.dispatch('surcharge/fetchAll', response)
-        this.$store.dispatch('discount/fetchAll', response)
-        this.$store.dispatch('customer/fetchAll', response)
-        this.$store.dispatch('payment/fetchAll', response)
-        this.$store.dispatch('giftcard/fetchAll', response)
-        this.$store.dispatch('invoice/fetchAll', response)
+            this.$store.dispatch('location/fetchAll', response)
+            this.$store.dispatch('announcement/fetchAll', response)
+            this.$store.dispatch('surcharge/fetchAll', response)
+            this.$store.dispatch('discount/fetchAll', response)
+            this.$store.dispatch('customer/fetchAll', response)
+            this.$store.dispatch('payment/fetchAll', response)
+            this.$store.dispatch('giftcard/fetchAll', response)
+            this.$store.dispatch('invoice/fetchAll', response)
+          })
+          .catch(err => (this.errored = err))
 
         // localStorage.setItem('selectedLanguage', this.defaultLanguage.language)
         // localStorage.setItem('selectedLanguageSortName', this.defaultLanguage.shortname)
       })
       .catch(error => (this.errored = error))
+      .finally(() => (this.loading = false))
   },
 
   mounted() {},
