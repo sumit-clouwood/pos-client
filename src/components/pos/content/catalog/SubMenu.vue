33915cd6d004267ada9a032fbb42df4edd8ffbd5
yuvraj.singh <yuvraj.singh@sunarctechnologies.com>
2019-04-02 14:03:32 +0530

    fixed sub_menu img issue

--- src/components/pos/content/catalog/SubMenu.vue
+++ src/components/pos/content/catalog/SubMenu.vue
@@ -7,6 +7,7 @@
     >
       <div @click.prevent="getItems(item)">
         <img
+          @error="imageLoadError(item.name.replace(/ /g,''))" :class="item.name.replace(/ /g,'')"
           :src="subcategoryImage(item.sub_category_image)"
           :alt="item.name"
         />
@@ -28,7 +29,21 @@ export default {
     }),
     ...mapGetters('category', ['subcategoryImage', 'subcategories']),
   },
-  methods: mapActions('category', ['getItems']),
+  methods: {
+    ...mapActions('category', ['getItems']),
+    imageLoadError (className) {
+      $('img.'+className).remove()
+      let hue = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ')'
+      $('div.vegetable:not(.pos-item-bg) p.remove-bottom, .pizza-size-wrapper > div:not(.pos-size-bg)').each(function() {
+        $(this).css('background-color', hue)
+      })
+    },
+  },
+
+  updated() {
+    $( '.vegetable:has(img)' ).addClass( 'pos-item-bg' )
+    $( '.pizza-size-wrapper > div:has(img)' ).addClass( 'pos-size-bg' )
+  }
 }
 </script>
 <style scoped lang="scss">
