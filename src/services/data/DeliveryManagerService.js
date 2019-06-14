af23ade2237f625b9d40ba017837f40b7f7dd47a
Tofeeq <tof33q@gmail.com>
2019-06-14 18:06:16 +0500

    Fixed /api routing with new backend routing

--- src/services/data/DeliveryManagerService.js
+++ src/services/data/DeliveryManagerService.js
@@ -3,38 +3,37 @@ import DataService from '@/services/DataService'
 export default {
   getDMOrderCount(...[location_id, order_status]) {
     return DataService.get(
-      `/api/auth/deliveryManager/get/order/?location_id=${location_id}&order_status=${order_status}`
+      `/auth/deliveryManager/get/order/?location_id=${location_id}&order_status=${order_status}`
     )
   },
 
   getDMOrderDetails(...[page_id, query, limit, page, orderBy]) {
     return DataService.get(
       //?page_id=home_delivery_new&query=&limit=99999999&ascending=1&page=1&byColumn=0&orderBy=real_created_datetime
-      // `/api/auth/deliveryManager/get/order/detail?location_id=${location_id}&order_status=${order_status}&collected=${collected}&source=new-pos`
       `/model/orders?page_id=${page_id}&query=${query}&limit=${limit}&ascending=1&page=${page}&byColumn=0&orderBy=${orderBy}`
     )
   },
   dispatchOrders(...[location_id, is_pagination, pageSize, pageNumber]) {
     return DataService.get(
-      `/api/auth/deliveryManager/dispatch/screen/?location_id=${location_id}&is_pagination=${is_pagination}&pagesize=${pageSize}&pagenumber=${pageNumber}`
+      `/auth/deliveryManager/dispatch/screen/?location_id=${location_id}&is_pagination=${is_pagination}&pagesize=${pageSize}&pagenumber=${pageNumber}`
     )
   },
 
   updateTakeAwayOrder(...[location_id, order_id]) {
     return DataService.get(
-      `/api/auth/order/dispatch/collected?location_id=${location_id}&order_id=${order_id}`
+      `/auth/order/dispatch/collected?location_id=${location_id}&order_id=${order_id}`
     )
   },
 
   getMoreOrders(...[location_id, driver_id]) {
     return DataService.get(
-      `api/auth/deliveryManager/delivered/showData?driver_id=${driver_id}&location_id=${location_id}`
+      `/auth/deliveryManager/delivered/showData?driver_id=${driver_id}&location_id=${location_id}`
     )
   },
 
   assignDriverToOrder(...[location_id, order_id, driver_id, timestamp]) {
     return DataService.post(
-      `/api/auth/deliveryManager/assign/driver/?location_id=${location_id}&order_id=${order_id}&driver_id=${driver_id}&timestamp=${timestamp}`
+      `/auth/deliveryManager/assign/driver/?location_id=${location_id}&order_id=${order_id}&driver_id=${driver_id}&timestamp=${timestamp}`
     )
   },
 
