0235daf7e1d6f8a16fc4bf49443825901eecc984
nsmodi <nidhishanker.modi@sunarctechnologies.com>
2019-06-18 17:17:59 +0530

    Filter All Item discounts based on their order type and country specific on POS. Filter All Order discounts based on country specific on POS. For Order type, API is not sending that parameters.

--- src/store/modules/discount.js
+++ src/store/modules/discount.js
@@ -55,17 +55,25 @@ const getters = {
   },
 
   itemDiscounts: (state, getters, rootState) => {
-    return state.itemDiscounts.data.filter(
-      discount => discount[rootState.order.orderType.OTApi]
-    )
+    return state.itemDiscounts.data.filter(function(q) {
+      let checkCountry = q.availability.incl.countries.includes(
+        rootState.location.store.country
+      )
+      if (checkCountry && q[rootState.order.orderType.OTApi] === true) {
+        return q
+      }
+    })
   },
 
-  orderDiscounts: state => {
-    return state.orderDiscounts.data
-    /*.filter(
-      discount => discount[rootState.order.orderType]
-    )
-    */
+  orderDiscounts: (state, getters, rootState) => {
+    return state.orderDiscounts.data.filter(function(q) {
+      let checkCountry = q.availability.incl.countries.includes(
+        rootState.location.store.country
+      )
+      if (checkCountry /*&& q[rootState.order.orderType.OTApi] === true*/) {
+        return q
+      }
+    })
   },
 }
 
