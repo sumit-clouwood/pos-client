a298ded0066cb2e10620926bc76a636448d518a1
yuvraj.singh <yuvraj.singh@sunarctechnologies.com>
2019-07-05 18:55:26 +0530

    DIM-336 fixed

--- src/components/deliveryManager/DMTopRightNav.vue
+++ src/components/deliveryManager/DMTopRightNav.vue
@@ -2,14 +2,11 @@
   <div class="page-header">
     <div class="current-time">
       <div class="header-main-left-time">
-        <a href="javascript:void(0)">
-          <span>{{ todayDate }}</span>
-          &nbsp;
-          <span class="time">{{ todayTime }}</span>
-        </a>
+        <span>{{ todayDate }} </span>
+        <span class="time"> {{ todayTime }}</span>
       </div>
     </div>
-    <ul>
+    <!--<ul class="hide">
       <li v-if="availableLanguages">
         <select
           v-model="vlocale"
@@ -25,7 +22,7 @@
           </option>
         </select>
       </li>
-    </ul>
+    </ul>-->
     <div class="button-block">
       <div class="change-location">
         <button class="btn btn-success">Change Brand</button>
@@ -33,24 +30,38 @@
           <router-link :to="store" class="text-white">Walk-in</router-link>
         </button>
       </div>
-      <div class="lang-selector">
-        <div class="v-menu v-menu--inline">
-          <div class="v-menu__activator">
-            <button type="button" class="v-btn v-btn--icon theme--light">
-              <div class="v-btn__content">
-                <div class="lang-flag-container">
-                  <img src="/img/icons/us.svg" />
-                </div>
-              </div>
-            </button>
-          </div>
-        </div>
+      <button
+        class="v-btn v-btn--icon theme--light dropdown-toggle lang-flag-container"
+        type="button"
+        id="dropdownLanguage"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
+        <img src="/img/icons/us.svg" />
+      </button>
+      <div
+        aria-labelledby="dropdownLanguage"
+        class="dropdown-menu cursor-pointer"
+        v-if="availableLanguages"
+        @change="changeLanguage(vlocale)"
+      >
+        <a
+          class="dropdown-item"
+          href="javascript:void(0)"
+          v-for="language in availableLanguages"
+          :key="language._id"
+          :value="language.code"
+        >
+          {{ language.name }}
+        </a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+// /* global $ */
 /* eslint-disable no-console */
 import moment from 'moment-timezone'
 import { mapGetters, mapState } from 'vuex'
@@ -67,11 +78,19 @@ export default {
       bootstrap.loadUI(this.$store)
       this.$store.dispatch('location/changeLanguage', locale)
     },
+    /*selectedReason: function(reason) {
+      this.showSelectedReason = reason.name
+      $('.dropdown-content').hide()
+    },
+    showDropdown: function() {
+      $('.dropdown-content').show()
+    },*/
   },
   data() {
     return {
       todayDate: moment().format('MMMM Do YYYY'),
       todayTime: moment().format('h:mm:ss a'),
+      showSelectedLanguage: '',
     }
   },
   mounted: function() {
