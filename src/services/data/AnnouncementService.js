01a6018b429678590d3f4b5ebe9b6cc905a2d24d
yuvraj.singh <yuvraj.singh@sunarctechnologies.com>
2019-07-08 17:05:18 +0530

    fixed ROLE

--- src/services/data/AnnouncementService.js
+++ src/services/data/AnnouncementService.js
@@ -3,7 +3,7 @@ import DataService from '@/services/DataService'
 export default {
   fetchAll(roleId) {
     return DataService.get(
-      `/model/brand_announcements?page_id=brand_announcements_main_tbl&query=&limit=10&ascending=1&page=1&byColumn=1&orderBy=announcement&brand_role=${roleId}`
+      `/model/brand_announcements?page_id=brand_announcements_main_tbl&query=&limit=10&ascending=1&page=1&byColumn=1&orderBy=announcement&role=${roleId}`
     )
   },
 }
