d5406428d9eb6aab8ed955cf0e71a18ef9574555
yuvraj.singh <yuvraj.singh@sunarctechnologies.com>
2019-07-16 19:10:26 +0530

    removed order status from delivery manager and order service

--- src/services/data/DeliveryManagerService.js
+++ src/services/data/DeliveryManagerService.js
@@ -2,11 +2,10 @@ import DataService from '@/services/DataService'
 
 export default {
   getDMOrderDetails(
-    // eslint-disable-next-line no-unused-vars
     ...[query, limit, orderBy, orderStatus, page, pageId, storeId]
   ) {
     return DataService.get(
-      `/model/orders?page_id=${pageId}&query=${query}&limit=${limit}&ascending=1&page=${page}&byColumn=0&orderBy=${orderBy}&store_id=${storeId}`
+      `/model/orders?page_id=${pageId}&query=${query}&limit=${limit}&ascending=1&page=${page}&byColumn=0&orderBy=${orderBy}&order_status=${orderStatus}&store_id=${storeId}`
     )
   },
 
