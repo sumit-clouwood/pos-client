0a7fbcbef569e83592b546218d43723557b91758
yuvraj.singh <yuvraj.singh@sunarctechnologies.com>
2019-07-24 17:40:03 +0530

    fixed brand setting URI

--- src/components/pos/header/TopNavRight.vue
+++ src/components/pos/header/TopNavRight.vue
@@ -79,7 +79,7 @@
           </a>
         </li>
         <li v-if="permitted('brand', 'root')">
-          <a :href="brands">{{ _t('Setting') }}</a>
+          <a :href="brand">{{ _t('Setting') }}</a>
         </li>
         <li>
           <a href="javascript:void(0)" @click="logout()">{{ _t('Logout') }}</a>
