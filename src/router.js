56e532bf6e2fbca4e41b2c86eea5e89a12a75644
yuvraj.singh <yuvraj.singh@sunarctechnologies.com>
2019-08-02 14:24:40 +0530

    added modyfy order

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home'
+import ModifyOrder from './views/ModifyOrder.vue'
 import DineIn from './views/Dinein'
 import DeliveryManagerInit from './views/DeliveryManagerInit'
 import DispatchScreenInit from './views/DispatchScreenInit'
@@ -23,6 +24,11 @@ const router = new Router({
       path: '/delivery-manager/:brand_id/:store_id/',
       name: 'DeliveryManager',
       component: DeliveryManagerInit,
+    },
+    {
+      path: '/:brand_id/:store_id/:order_id',
+      name: 'ModifyOrder',
+      component: ModifyOrder,
     },
     {
       path: '/about',
