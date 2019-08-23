0b3726623537185544d28d5a314afe3cfaaefbbe
yuvraj.singh <yuvraj.singh@sunarctechnologies.com>
2019-08-23 12:13:01 +0530

    zoom in, zoom out done with moving tables in all directions

--- src/components/dinein/content/TableStatus.vue
+++ src/components/dinein/content/TableStatus.vue
@@ -18,21 +18,27 @@
     </div>
     <div class="zoom-wrap-dinein">
       <div class="POSItemOptions_quantity_inputs">
-        <button class="qtyminus value-qty">-</button>
-        <input class="qty" min="1" name="quantity" type="text" value="100" />
-        <button class="qtyplus value-qty">+</button>
+        <button class="qtyminus value-qty" @click="zoomOut()">-</button>
+        <input
+          class="qty"
+          min="1"
+          name="quantity"
+          type="text"
+          :value="zoomPercent"
+        />
+        <button class="qtyplus value-qty" @click="zoomIn()">+</button>
       </div>
       <div class="round-dinein-circle">
-        <span id="dine-top" @click="moveTable('-=50', 'top')">
+        <span id="dine-top" @click="moveUp()">
           <img src="img/dinein/chairs/dine-top.svg" />
         </span>
-        <span id="dine-left" @click="moveTable('-=50', 'left')">
+        <span id="dine-left" @click="moveLeft()">
           <img src="img/dinein/chairs/dine-left.svg" />
         </span>
-        <span id="dine-right" @click="moveTable('+=50', 'right')">
+        <span id="dine-right" @click="moveRight()">
           <img src="img/dinein/chairs/dine-right.svg" />
         </span>
-        <span id="dine-bottom" @click="moveTable('+=50', 'bottom')">
+        <span id="dine-bottom" @click="moveDown()">
           <img src="img/dinein/chairs/dine-bottom.svg" />
         </span>
         <div class="inner-round">
@@ -45,15 +51,85 @@
 
 <script>
 import { mapState } from 'vuex'
-/* global moveTables */
+import * as d3 from 'd3'
+
 export default {
   name: 'TableStatus',
   computed: {
     ...mapState('dinein', ['tableStatus']),
   },
+  data() {
+    return {
+      transform: {
+        x: 0,
+        y: 0,
+        k: 1,
+      },
+      zoomPercent: 100,
+    }
+  },
   methods: {
-    moveTable(px, movingSide) {
-      moveTables(px, movingSide)
+    zoomIn() {
+      let that = this
+      that.transform.k += 0.2
+      d3.selectAll('.dinein_table_parent').each((d, i, a) => {
+        let transform = d3.zoomIdentity
+          .scale(that.transform.k)
+          .translate(that.transform.x, that.transform.y)
+        that.zoomPercent += 10
+        d3.select(a[i]).attr('transform', transform)
+      })
+    },
+    zoomOut() {
+      let that = this
+      that.transform.k -= 0.2
+      d3.selectAll('.dinein_table_parent').each((d, i, a) => {
+        let transform = d3.zoomIdentity
+          .scale(that.transform.k)
+          .translate(that.transform.x, that.transform.y)
+        that.zoomPercent -= 10
+        d3.select(a[i]).attr('transform', transform)
+      })
+    },
+    moveRight() {
+      let that = this
+      that.transform.x += 10
+      d3.selectAll('.dinein_table_parent').each((d, i, a) => {
+        let transform = d3.zoomIdentity
+          .scale(that.transform.k)
+          .translate(that.transform.x, that.transform.y)
+        d3.select(a[i]).attr('transform', transform)
+      })
+    },
+    moveLeft() {
+      let that = this
+      that.transform.x -= 10
+      d3.selectAll('.dinein_table_parent').each((d, i, a) => {
+        let transform = d3.zoomIdentity
+          .scale(that.transform.k)
+          .translate(that.transform.x, that.transform.y)
+        d3.select(a[i]).attr('transform', transform)
+      })
+    },
+    moveUp() {
+      let that = this
+      that.transform.y -= 10
+      d3.selectAll('.dinein_table_parent').each((d, i, a) => {
+        let transform = d3.zoomIdentity
+          .scale(that.transform.k)
+          .translate(that.transform.x, that.transform.y)
+        d3.select(a[i]).attr('transform', transform)
+      })
+    },
+    moveDown() {
+      let that = this
+      that.transform.y += 10
+      d3.selectAll('.dinein_table_parent').each((d, i, a) => {
+        let transform = d3.zoomIdentity
+          .scale(that.transform.k)
+          .translate(that.transform.x, that.transform.y)
+        d3.select(a[i]).attr('transform', transform)
+      })
     },
     getAvailableTableCount(tableStatus) {
       let tableCount = 0
