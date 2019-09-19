61689620a04c9ed09a7e2e1f50bef006d11e947c
Tofeeq <tof33q@gmail.com>
2019-09-19 20:19:35 +0500

    Showing amount change popup in dine in

--- src/store/modules/checkout.js
+++ src/store/modules/checkout.js
@@ -13,6 +13,7 @@ const state = {
   print: false,
   loading: false,
   orderNumber: null,
+  changeAmountStatus: false,
 }
 
 // getters
@@ -809,6 +810,9 @@ const mutations = {
   },
   [mutation.LOADING](state, loadingStatus) {
     state.loading = loadingStatus
+  },
+  [mutation.CHANGE_AMOUNT_STATUS](state, status) {
+    state.changeAmountStatus = status
   },
   [mutation.RESET](state) {
     state.order = false
