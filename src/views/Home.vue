525fd7f5a42c317a05fc5bc292cdacba9156ccbb
Tofeeq <tof33q@gmail.com>
2019-10-09 10:51:44 +0500

    cashier pin login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <Location v-show="!loaded" msg="Broccoli POS" />
-    <Pos v-show="loaded" msg="Broccoli POS" />
+    <Pos v-show="loaded && cashier" msg="Broccoli POS" />
+    <cashier v-show="loaded && !cashier" />
   </div>
 </template>
 
@@ -9,15 +10,22 @@
 // @ is an alias to /src
 /* global $ */
 import Pos from '@/components/Pos.vue'
+import Cashier from '@/components/login/Cashier.vue'
 import Location from '@/components/Location.vue'
 import { mapState } from 'vuex'
 // import BrandColor from '@/plugins/helpers/BrandColor'
 
 export default {
   name: 'home',
+  data() {
+    return {
+      cashier: false,
+    }
+  },
   components: {
     Pos,
     Location,
+    Cashier,
   },
   computed: {
     ...mapState('sync', ['loaded']),
