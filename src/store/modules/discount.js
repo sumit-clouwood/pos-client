fddcf40f3af3c05c26097a2c07f97651221327ae
Tofeeq <tof33q@gmail.com>
2019-10-10 14:26:51 +0500

    Fixed for items discount

--- src/store/modules/discount.js
+++ src/store/modules/discount.js
@@ -70,10 +70,12 @@ const getters = {
     }
     return state.itemDiscounts.data.filter(discount => {
       if (discount[rootState.order.orderType.OTApi]) {
-        return Availability.available(
-          discount,
-          rootState.location.timezoneString
-        )
+        if (discount.for_items.includes(rootState.order.item._id)) {
+          return Availability.available(
+            discount,
+            rootState.location.timezoneString
+          )
+        }
       }
     })
   },
