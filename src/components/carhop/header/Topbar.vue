eeb11a70ef28906c1e892e5b37105b77d431bed7
Tofeeq <tof33q@gmail.com>
2019-10-22 14:57:57 +0500

    added buttons

--- src/components/carhop/header/Topbar.vue
+++ src/components/carhop/header/Topbar.vue
@@ -1,17 +1,49 @@
 <template>
-  <div class="header-main-right color-dashboard-background"></div>
+  <div class="header-main-right color-dashboard-background">
+    <button
+      type=""
+      class="tables-btn-style"
+      :class="{ active: orderType === 'running' }"
+      @click="
+        fetchOrders({
+          type: 'running',
+          pageId: 'carhopInRunningOrders',
+        })
+      "
+    >
+      {{ _t('Running Orders') }}
+    </button>
+    <button
+      type=""
+      class="tables-btn-style"
+      :class="{ active: orderType === 'completed' }"
+      @click="
+        fetchOrders({
+          type: 'completed',
+          pageId: 'carhopCompleteOrders',
+        })
+      "
+    >
+      {{ _t('Completed Orders') }}
+    </button>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 //import { mapState, mapGetters, mapActions } from 'vuex'
 export default {
   name: 'TopNavRight',
   props: {},
   data: function() {
-    return {}
+    return {
+      orderType: 'running',
+    }
   },
   updated() {},
-  computed: {},
+  computed: {
+    ...mapGetters('location', ['_t']),
+  },
   methods: {},
   mounted() {},
 }
