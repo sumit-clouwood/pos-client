40a4e8bd48f2c5f1a1043eb0986a88482671d0ab
Tofeeq <tof33q@gmail.com>
2019-10-24 12:00:51 +0500

    less frequently show the update available message

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,8 +3,16 @@ import { register } from 'register-service-worker'
 import store from './store'
 // eslint-disable-next-line no-unused-vars
 const notifyUserAboutUpdate = worker => {
-  store.commit('sync/setAppUpdateNotification', true)
-  localStorage.setItem('update_available', true)
+  const today = new Date()
+  const date =
+    today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
+  const time = today.getHours()
+  const dateTime = date + ' ' + time
+  if (localStorage.getItem('pos_version_updated_on') != dateTime) {
+    localStorage.setItem('pos_version_updated_on', dateTime)
+    store.commit('sync/setAppUpdateNotification', true)
+    localStorage.setItem('update_available', true)
+  }
 }
 
 if (process.env.NODE_ENV === 'production') {
