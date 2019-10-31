b6160ef59c559cef6ea9bbed1f362d96b385130a
yuvraj.singh <yuvraj.singh@sunarctechnologies.com>
2019-10-31 14:02:44 +0530

    set opacity on loader

--- src/components/dinein/content/TableDraw.vue
+++ src/components/dinein/content/TableDraw.vue
@@ -419,6 +419,10 @@ export default {
         .hide()
     },
     newOrder(reservationId, pos) {
+      let makeId = '#id_' + this.selectedTableId
+      $(makeId)
+        .find('g')
+        .attr('style', 'opacity:0.5')
       this.$store.commit('dinein/TABLE_SPLIT', false)
       this.$store.commit('dinein/SELECTED_TABLE', this.selectedTableData)
       if (!reservationId) {
@@ -438,6 +442,11 @@ export default {
                 this.$store.dispatch('dinein/getDineInArea', false)
                 this.$store.dispatch('dinein/getDineInTables', false)
                 $('#tooltipdata').hide()
+                setTimeout(function() {
+                  $(makeId)
+                    .find('g')
+                    .removeAttr('style')
+                }, 450)
               })
           })
       } else {
