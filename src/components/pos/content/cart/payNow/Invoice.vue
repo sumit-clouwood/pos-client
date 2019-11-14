249734869d6e5aad1b02a6c6be91fe0f6bc3ae91
Tofeeq <tof33q@gmail.com>
2019-11-14 19:06:35 +0500

    complete print split

--- src/components/pos/content/cart/payNow/Invoice.vue
+++ src/components/pos/content/cart/payNow/Invoice.vue
@@ -47,6 +47,7 @@ export default {
       'changedAmount',
       'changeAmountStatus',
       'paymentMsgStatus',
+      'paymentAction',
     ]),
     ...mapState('context', ['brandId']),
     ...mapGetters('invoice', ['template']),
@@ -119,11 +120,12 @@ export default {
             //   this.$store.dispatch('checkout/splitOrder').then(() => {})
             // }
             //Invoice APP API Call with Custom Request JSON
-            // commit('checkout/SET_PROCESSING', false, { root: true })
-            this.$store.dispatch(
-              'printingServer/printingServerInvoiceRaw',
-              orderData
-            )
+            if (!['dine-in-order-preview'].includes(this.paymentAction)) {
+              this.$store.dispatch(
+                'printingServer/printingServerInvoiceRaw',
+                orderData
+              )
+            }
           }, 500)
           //this.$refs.iframe.contentWindow.print()
         } catch (e) {
