195747c3290947e5a03ba9a5409525b94b63773a
yuvraj.singh <yuvraj.singh@sunarctechnologies.com>
2019-11-28 11:11:39 +0530

    New reservation popup auto close after reservation done Currently, showing related country code, after that I will add dropdown and popup there

--- src/components/dinein/content/reservation/popup/NewReservation.vue
+++ src/components/dinein/content/reservation/popup/NewReservation.vue
@@ -162,7 +162,7 @@
                   <div class="form-group">
                     <label>Phone</label>
                     <div class="input-group">
-                      <span class="lbl-txt-box">IN</span>
+                      <span class="lbl-txt-box">{{ brand.country }}</span>
                       <input
                         type="text"
                         class="form-control txt-box"
@@ -224,8 +224,10 @@
                       >
                         <th scope="row">{{ reservations.start_date }}</th>
                         <td>{{ reservations.start_time }}</td>
-                        <td>{{ reservations.status }}</td>
-                        <td>Victoria Pope Daniel</td>
+                        <td class="text-capitalize">
+                          {{ reservations.status }}
+                        </td>
+                        <td class="text-capitalize">NA</td>
                       </tr>
                     </tbody>
                     <tbody v-else>
@@ -479,7 +481,7 @@ export default {
         })
         .then(response => {
           this.errors = response.data.form_errors || false
-          $('#newReservation').modal('hide')
+          $('#NewReservation').modal('hide')
           // eslint-disable-next-line no-console
           console.log(response.data, this.errors, this.errors['guest_email'])
         })
