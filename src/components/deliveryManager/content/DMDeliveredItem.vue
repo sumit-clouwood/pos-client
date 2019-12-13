31482762d6ee2390a42cdb0667aa4d7ccb297dff
yuvraj.singh <yuvraj.singh@sunarctechnologies.com>
2019-12-13 19:09:29 +0530

    fixed delivered order UI

--- src/components/deliveryManager/content/DMDeliveredItem.vue
+++ src/components/deliveryManager/content/DMDeliveredItem.vue
@@ -105,24 +105,8 @@
                 class="btn btn-info btn-large btnShowDetails btn-show-details-delivered"
                 @click="showOrders(driOrders.driverId)"
               >
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="13"
-                    height="7"
-                    viewBox="0 0 20 13"
-                  >
-                    <g fill="#fff" fill-rule="nonzero">
-                      <path
-                        d="M10 12.971c-2.229 0-4.621-1.053-6.921-3.063A19.21 19.21 0 0 1 .263 6.884a.526.526 0 0 1 0-.608A19.21 19.21 0 0 1 3.08 3.25C5.379 1.245 7.77.187 10 .187s4.621 1.052 6.921 3.063a19.21 19.21 0 0 1 2.816 3.024.526.526 0 0 1 0 .608 19.21 19.21 0 0 1-2.816 3.026c-2.3 2.005-4.692 3.063-6.921 3.063zM1.363 6.58c.924 1.174 4.492 5.34 8.637 5.34 4.145 0 7.716-4.164 8.637-5.34-.924-1.174-4.492-5.34-8.637-5.34-4.145 0-7.716 4.164-8.637 5.34z"
-                      />
-                      <path
-                        d="M10 10.39a3.81 3.81 0 1 1 3.713-3.811A3.766 3.766 0 0 1 10 10.389zm0-6.58a2.758 2.758 0 1 0 2.66 2.769A2.713 2.713 0 0 0 10 3.82v-.01z"
-                      />
-                    </g>
-                  </svg>
-                </span>
-                {{ _t('Show Details') }}
+                <i class="fa fa-eye" aria-hidden="true"></i>
+                <!--{{ _t('Show Details') }}-->
               </a>
               <a
                 :class="'driver_details_hide-' + driOrders.driverId"
@@ -130,8 +114,8 @@
                 class="btn btn-info btn-large btnShowDetails btn-show-details"
                 style="display: none;"
                 @click="showOrders(driOrders.driverId)"
-                >{{ _t('Hide') }}</a
-              >
+                ><i class="fa fa-eye-slash" aria-hidden="true"></i>
+              </a>
             </td>
           </tr>
           <tr
@@ -299,6 +283,12 @@ export default {
 }
 </script>
 <style lang="scss">
+tr.dataContentStyle > td > a svg {
+  width: 20px;
+  height: 12px;
+  display: inline-block;
+  vertical-align: middle;
+}
 .align-right {
   a {
     font-size: 12px;
