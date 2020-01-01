4718806b140762e91a4090bd0a17a7ba51f8bee4
Tofeeq <tof33q@gmail.com>
2020-01-01 16:40:22 +0500

    fixed routing navigation

--- src/PrivateView.vue
+++ src/PrivateView.vue
@@ -171,35 +171,43 @@ export default {
   watch: {
     // eslint-disable-next-line no-unused-vars
     $route(to, from) {
-      this.$store.dispatch('checkout/reset')
-      this.$store.commit('order/RESET_SPLIT_BILL')
-
-      let orderType = {
-        OTview: 'Walk In',
-        OTApi: 'walk_in',
-      }
-      //{ OTview: 'Delivery', OTApi: 'call_center' }
-      switch (to.name) {
-        case 'Dinein':
-          orderType = {
-            OTview: 'Dine In',
-            OTApi: 'dine_in',
-          }
-          break
-        case 'Carhop':
-          orderType = {
-            OTview: 'Carhop',
-            OTApi: CONST.ORDER_TYPE_CARHOP,
-          }
-          break
-      }
-      this.$store.commit('order/ORDER_TYPE', orderType)
-      // react to route changes...
-      setTimeout(() => {})
-
       if (this.$route.params.order_id) {
         this.orderId = this.$route.params.order_id
         this.$store.commit('order/RESET_SPLIT_BILL')
+      }
+      //check if orderid exists from previous action
+      if (this.orderId) {
+        this.$store.dispatch('checkout/reset')
+      }
+      // if no order id found in current action reset this.orderId
+      if (!this.$route.params.order_id) {
+        this.orderId = null
+      }
+
+      this.$store.commit('order/RESET_SPLIT_BILL')
+      if (to.name !== 'setOrderType') {
+        //set order type is already set in dinin.vue, don't override here
+
+        let orderType = {
+          OTview: 'Walk In',
+          OTApi: 'walk_in',
+        }
+        //{ OTview: 'Delivery', OTApi: 'call_center' }
+        switch (to.name) {
+          case 'Dinein':
+            orderType = {
+              OTview: 'Dine In',
+              OTApi: 'dine_in',
+            }
+            break
+          case 'Carhop':
+            orderType = {
+              OTview: 'Carhop',
+              OTApi: CONST.ORDER_TYPE_CARHOP,
+            }
+            break
+        }
+        this.$store.commit('order/ORDER_TYPE', orderType)
       }
 
       if (this.$route.params.table_id) {
