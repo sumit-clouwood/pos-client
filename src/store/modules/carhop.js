5019dc60be2b09243e8082a3069820a782270118
Abdul Jalil Ashraf <abdul.ashraf.jalil@gmail.com>
2020-01-06 11:25:14 +0500

    fix pagination issue for self placed orders module

--- src/store/modules/carhop.js
+++ src/store/modules/carhop.js
@@ -1,6 +1,6 @@
 import * as mutation from './carhop/mutation-types'
 import CarhopService from '@/services/data/CarhopService'
-import OrderHelper from '@/plugins/helpers/Order'
+// import OrderHelper from '@/plugins/helpers/Order'
 import * as PERMS from '@/const/permissions'
 
 const state = {
@@ -35,18 +35,25 @@ const actions = {
 
     commit(mutation.SET_LOADING_SILENT, true)
 
-    CarhopService.fetchOrders(state.orderStatus, 1, state.limit).then(
+    //check if allowed to see others orders
+    let userId = ''
+    if (!rootGetters['auth/allowed'](PERMS.SEE_OTHERS_ORDERS)) {
+      userId = rootState.auth.userDetails.item._id
+    }
+
+    CarhopService.fetchOrders(state.orderStatus, 1, state.limit, userId).then(
       response => {
         if (response.data) {
           let orders = response.data
-          //check if allowed to see others orders
-          if (!rootGetters['auth/allowed'](PERMS.SEE_OTHERS_ORDERS)) {
-            orders.data = OrderHelper.userOrders(
-              response.data.data,
-              rootState.auth.userDetails.item._id
-            )
-          }
-
+          // check if allowed to see others orders
+          // if (!rootGetters['auth/allowed'](PERMS.SEE_OTHERS_ORDERS)) {
+          //   orders.data = OrderHelper.userOrders(
+          //     response.data.data,
+          //     rootState.auth.userDetails.item._id
+          //   )
+          // }
+          // /* eslint-disable */
+          // console.log(orders.data.length)
           commit(mutation.SET_ORDERS, {
             data: orders,
             type: 'in-progress',
@@ -56,21 +63,23 @@ const actions = {
         commit(mutation.SET_LOADING_SILENT, false)
       }
     )
-    CarhopService.fetchOrders('finished', 1, state.limit).then(response => {
-      if (response.data) {
-        let orders = response.data
-        //check if allowed to see others orders
-        if (!rootGetters['auth/allowed'](PERMS.SEE_OTHERS_ORDERS)) {
-          orders.data = OrderHelper.userOrders(
-            response.data.data,
-            rootState.auth.userDetails.item._id
-          )
+    CarhopService.fetchOrders('finished', 1, state.limit, userId).then(
+      response => {
+        if (response.data) {
+          let orders = response.data
+          //check if allowed to see others orders
+          // if (!rootGetters['auth/allowed'](PERMS.SEE_OTHERS_ORDERS)) {
+          //   orders.data = OrderHelper.userOrders(
+          //     response.data.data,
+          //     rootState.auth.userDetails.item._id
+          //   )
+          // }
+          commit(mutation.SET_ORDERS, { data: orders, type: 'finished' })
         }
-        commit(mutation.SET_ORDERS, { data: orders, type: 'finished' })
+        commit(mutation.SET_LOADING, false)
+        commit(mutation.SET_LOADING_SILENT, false)
       }
-      commit(mutation.SET_LOADING, false)
-      commit(mutation.SET_LOADING_SILENT, false)
-    })
+    )
   },
 
   fetchOrders(
@@ -79,16 +88,22 @@ const actions = {
   ) {
     commit(mutation.SET_LOADING_SILENT, true)
     commit(mutation.SET_ORDER_STATUS, orderStatus)
-    CarhopService.fetchOrders(orderStatus, page, state.limit).then(response => {
+    //check if allowed to see others orders
+    let userId = ''
+    if (!rootGetters['auth/allowed'](PERMS.SEE_OTHERS_ORDERS)) {
+      /* eslint-disable */
+      userId = rootState.auth.userDetails.item._id
+    }
+    CarhopService.fetchOrders(orderStatus, page, state.limit, userId).then(response => {
       if (response.data) {
         let orders = response.data
         //check if allowed to see others orders
-        if (!rootGetters['auth/allowed'](PERMS.SEE_OTHERS_ORDERS)) {
-          orders.data = OrderHelper.userOrders(
-            response.data.data,
-            rootState.auth.userDetails.item._id
-          )
-        }
+        // if (!rootGetters['auth/allowed'](PERMS.SEE_OTHERS_ORDERS)) {
+        //   orders.data = OrderHelper.userOrders(
+        //     response.data.data,
+        //     rootState.auth.userDetails.item._id
+        //   )
+        // }
         commit(mutation.SET_ORDERS, { data: orders, type: orderStatus })
         commit(mutation.SET_PAGE, { page: page, type: orderStatus })
       }
