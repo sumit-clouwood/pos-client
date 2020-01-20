8effd3ea03a45a093896617571d379c305756242
sunarcdell <yuvraj.singh@sunarctechnologies.com>
2020-01-20 16:35:25 +0530

    added referral in order details popup

--- src/components/pos/content/orderDetails/rightContent/Payment.vue
+++ src/components/pos/content/orderDetails/rightContent/Payment.vue
@@ -5,43 +5,81 @@
     role="tabpanel"
     aria-labelledby="nav-payments-tab"
   >
-    <table
-      class="table-responsive"
-      v-if="orderDetails && orderDetails.order_payments.length"
-    >
-      <tr class="receipt-body color-secondary color-text-invert">
-        <th class="receipt-heading">{{ _t('Payment Method') }}</th>
-        <th class="receipt-heading">{{ _t('Collected') }}</th>
-      </tr>
-      <tr
-        class="receipt-body color-tables-background color-text"
-        v-for="(item, index) in orderDetails.order_payments"
-        :key="index"
+    {{ getReferral() }}
+    <div v-if="orderDetails">
+      <table
+        class="table-responsive"
+        v-if="appliedReferral && orderDetails.order_type === 'call_center'"
       >
-        <td>{{ item.name }}</td>
-        <td class="base-price">{{ item.collected }}</td>
-      </tr>
-    </table>
+        <tr class="receipt-body color-secondary color-text-invert">
+          <th class="receipt-heading">{{ _t('Payment Method') }}</th>
+          <!--<th class="receipt-heading">{{ _t('Referral Type') }}</th>-->
+          <th class="receipt-heading">{{ _t('Collected') }}</th>
+        </tr>
+        <tr class="receipt-body color-tables-background color-text">
+          <td>{{ appliedReferral.name }}</td>
+          <!--<td>{{ appliedReferral.referral_type }}</td>-->
+          <td class="base-price">
+            {{ orderDetails.balance_due }}
+          </td>
+        </tr>
+      </table>
+
+      <table
+        class="table-responsive"
+        v-else-if="orderDetails.order_payments.length"
+      >
+        <tr class="receipt-body color-secondary color-text-invert">
+          <th class="receipt-heading">{{ _t('Payment Method') }}</th>
+          <th class="receipt-heading">{{ _t('Collected') }}</th>
+        </tr>
+        <tr
+          class="receipt-body color-tables-background color-text"
+          v-for="(item, index) in orderDetails.order_payments"
+          :key="index"
+        >
+          <td>{{ item.name }}</td>
+          <td class="base-price">{{ item.collected }}</td>
+        </tr>
+      </table>
+
+      <div v-else>
+        <h5 class="text-center pt-2 pb-3">{{ _t('No matching records') }}s</h5>
+      </div>
+    </div>
 
     <div v-else>
       <h5 class="text-center pt-2 pb-3">{{ _t('No matching records') }}s</h5>
     </div>
 
-    <div class="receipt-summary" v-if="orderDetails">
-      <div class="caption subtotal color-text-invert">
-        {{ _t('Total Paid') }}:
+    <div v-if="orderDetails">
+      <div
+        class="receipt-summary"
+        v-if="appliedReferral && orderDetails.order_type === 'call_center'"
+      >
+        <div class="caption subtotal color-text-invert">
+          {{ _t('Total Paid') }}:
+        </div>
+        <div class="subtotal color-text">
+          {{ formatPrice(orderDetails.balance_due) }}
+        </div>
       </div>
-      <div class="subtotal color-text">
-        {{ orderDetails.total_paid }}
-      </div>
-      <!---->
-      <div class="caption color-text-invert">{{ _t('Amount Changed') }}:</div>
-      <div class="color-text">
-        {{ orderDetails.amount_changed }}
-      </div>
-      <div class="caption color-text-invert">{{ _t('Tips') }}:</div>
-      <div class="color-text">
-        {{ orderDetails.tip_amount }}
+      <div class="receipt-summary" v-else>
+        <div class="caption subtotal color-text-invert">
+          {{ _t('Total Paid') }}:
+        </div>
+        <div class="subtotal color-text">
+          {{ formatPrice(orderDetails.total_paid) }}
+        </div>
+        <!---->
+        <div class="caption color-text-invert">{{ _t('Amount Changed') }}:</div>
+        <div class="color-text">
+          {{ orderDetails.amount_changed }}
+        </div>
+        <div class="caption color-text-invert">{{ _t('Tips') }}:</div>
+        <div class="color-text">
+          {{ orderDetails.tip_amount }}
+        </div>
       </div>
     </div>
   </div>
@@ -53,9 +91,23 @@ export default {
   name: 'Payment',
   props: {
     orderDetails: {},
+    lookups: {},
+  },
+  data() {
+    return {
+      appliedReferral: false,
+    }
   },
   computed: {
-    ...mapGetters('location', ['_t']),
+    ...mapGetters('location', ['_t', 'formatPrice']),
+  },
+  methods: {
+    getReferral() {
+      if (this.lookups && typeof this.lookups.brand_referrals !== 'undefined') {
+        this.appliedReferral =
+          this.lookups.brand_referrals._id[this.orderDetails.referral] || false
+      }
+    },
   },
 }
 </script>
