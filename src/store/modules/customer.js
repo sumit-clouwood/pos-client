821138c7de3316d8161511a1d2ef633a8d322014
Abdul Jalil Ashraf <abdul.ashraf.jalil@gmail.com>
2020-01-30 19:01:02 +0500

    loyalty should be added without any check

--- src/store/modules/customer.js
+++ src/store/modules/customer.js
@@ -555,6 +555,7 @@ const mutations = {
   },
   [mutation.LOYALTY_FILTER](
     state,
+    // eslint-disable-next-line no-unused-vars
     { loyalty, orderType, orderCurrency, orderAmount } = {
       loyalty: null,
       orderType: null,
@@ -591,11 +592,12 @@ const mutations = {
         state.loyalty.card = false
         state.loyalty.details = false
         state.loyalty.points = false
-      } else if (state.loyalty.details.min_order > orderAmount) {
-        state.loyalty.card = false
-        state.loyalty.details = false
-        state.loyalty.points = false
       } else {
+        // if (state.loyalty.details.min_order > orderAmount) {
+        //   state.loyalty.card = false
+        //   state.loyalty.details = false
+        //   state.loyalty.points = false
+        // } else {
         /* do noting */
       }
     } else {
