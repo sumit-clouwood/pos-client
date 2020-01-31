e8cc8ba2a25b1bf2068fced01335e4efcc3e9501
sunarcdell <yuvraj.singh@sunarctechnologies.com>
2020-01-31 17:49:20 +0530

    comment moment

--- src/store/modules/printingServer.js
+++ src/store/modules/printingServer.js
@@ -2,7 +2,7 @@ import * as mutation from './printingServer/mutation-type'
 // import OrderService from '@/services/data/OrderService'
 import PrintingServerService from '@/services/data/PrintingServerService'
 // import LookupData from '@/plugins/helpers/LookupData'
-import moment from 'moment-timezone'
+// import moment from 'moment-timezone'
 import { compressToBase64 } from 'lz-string'
 const state = {
   kitchenitems: [],
