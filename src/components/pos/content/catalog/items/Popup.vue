995aeebd38540f9dbe746c632271ebb427f149e0
Abdul Jalil Ashraf <abdul.ashraf.jalil@gmail.com>
2020-02-10 19:48:20 +0500

    fix modifiers popup

--- src/components/pos/content/catalog/items/Popup.vue
+++ src/components/pos/content/catalog/items/Popup.vue
@@ -72,7 +72,9 @@ export default {
           border: none;
           z-index: 1;
           background-color: #fff;
-
+          display: flex;
+          flex-wrap: wrap;
+          flex-direction: column-reverse;
           .POSItemOptions_pricequantity {
             .POSItemOptions_price {
               .POSItemOptions_money {
@@ -85,7 +87,7 @@ export default {
               bottom: 0;
               right: 0;
               left: 0;
-              padding: 1em;
+              padding: 0.75vh;
               box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
               width: 100%;
               background-color: #fff;
@@ -98,7 +100,7 @@ export default {
                   button {
                     height: 50px;
                     background-color: $green-middle;
-                    width: auto;
+                    width: auto !important;
                     padding: 0 25px;
                   }
                 }
