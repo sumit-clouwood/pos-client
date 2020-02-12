e1f316705cef6cd7df606c5dcfa7e4d2f2ac069c
Tofeeq <tof33q@gmail.com>
2020-02-12 22:20:33 +0500

    make use of caching to load contents

--- src/services/service-worker.js
+++ src/services/service-worker.js
@@ -2,8 +2,8 @@
 /* global workbox */
 /* eslint-disable no-console */
 //appVersion has production build number . staging build number . int build number . bugfix
-//Reason: Testing the new update
-var appVersion = '6.7.33.2'
+//Reason: Make use of caching
+var appVersion = '6.8.33.2'
 
 var clientUrl = ''
 
