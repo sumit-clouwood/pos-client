02479c903b8752f8238d9988f98ec95c8bac90cb
sunarcdell <yuvraj.singh@sunarctechnologies.com>
2020-02-25 11:16:43 +0530

    fixed UI

--- src/components/pos/footer/popups/BusinessSummary.vue
+++ src/components/pos/footer/popups/BusinessSummary.vue
@@ -8,7 +8,19 @@
           <h4 class="customer-title color-text-invert">
             {{ _t('Business Summary Details') }}
           </h4>
-          <div class="printConfg">
+          <div>
+            <label class="container-checkbox"
+              >Store Time
+              <input
+                type="checkbox"
+                checked="checked"
+                @click="getBSStoreTime"
+                v-model="timeMode"
+              />
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          <!--<div class="printConfg">
             <label>
               <input
                 @click="getBSStoreTime"
@@ -19,7 +31,7 @@
               />
             </label>
             <label> Store</label>
-          </div>
+          </div>-->
         </div>
         <div class="modal-body row business-summary">
           <div class="business-summary-wrapper">
@@ -280,3 +292,66 @@ export default {
   },
 }
 </script>
+<style>
+/* The container */
+.container-checkbox {
+  display: block;
+  position: relative;
+  padding-left: 26px;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default checkbox */
+.container-checkbox input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+}
+
+/* Create a custom checkbox */
+.container-checkbox .checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #eee;
+}
+
+/* On mouse-over, add a grey background color */
+.container-checkbox:hover input ~ .checkmark {
+  background-color: #ccc;
+}
+
+/* When the checkbox is checked, add a blue background */
+.container-checkbox input:checked ~ .checkmark {
+  background-color: #5056ca;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.container-checkbox .checkmark:after {
+  content: '';
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.container-checkbox input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the checkmark/indicator */
+.container-checkbox .checkmark:after {
+  left: 7px;
+  top: 3px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+</style>
