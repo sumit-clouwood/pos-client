f1b90277848ccc8f87d7d13891b6e55fd02dcbb0
Tofeeq <tof33q@gmail.com>
2020-03-12 10:38:01 +0500

    updated inventory related errors

--- src/store/modules/checkout.js
+++ src/store/modules/checkout.js
@@ -1462,7 +1462,11 @@ const actions = {
     let error = ''
     if (response.data.status == 'form_errors') {
       for (let i in response.data.form_errors) {
-        response.data.form_errors[i].forEach(err => (error += ' ' + err))
+        if (typeof response.data.form_errors[i] === 'string') {
+          error += ' ' + response.data.form_errors[i]
+        } else {
+          response.data.form_errors[i].forEach(err => (error += ' ' + err))
+        }
       }
     } else {
       error =
