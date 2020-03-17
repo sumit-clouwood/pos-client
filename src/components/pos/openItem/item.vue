81e471030430c282b8d05639f54a48c94ed9d6d9
sunarcdell <yuvraj.singh@sunarctechnologies.com>
2020-03-17 10:59:04 +0530

    merged open item

--- src/components/pos/openItem/item.vue
+++ src/components/pos/openItem/item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="modal fade" id="open-item" role="dialog" v-if="item">
+  <div class="modal fade" id="open-item" role="dialog">
     <div class="modal-dialog">
       <div class="modal-content color-dashboard-background">
         <div class="modal-header customer-header color-secondary">
@@ -10,32 +10,29 @@
         <div class="modal-body add-note-wrap ">
           <form autocomplete="off">
             <div class="add-note-area ">
+              <p class="color-text">
+                {{ _t('Item Name') }}
+              </p>
+              <input
+                      type="text"
+                      class="open-item name"
+                      v-model="item.name"
+                      placeholder="Name"
+                      autocomplete="off"
+              />
+              <div class="validation-error" v-if="errors.name">
+                {{ errors.name }}
+              </div>
               <p class="color-text">{{ _t('Price') }} ( {{ currency }} )</p>
               <input
-                type="text"
-                class="open-item price"
-                v-model.number="vitem.value"
-                placeholder="0.00"
-                autocomplete="off"
+                      type="text"
+                      class="open-item price"
+                      v-model="item.value"
+                      placeholder="0.00"
+                      autocomplete="off"
               />
               <div class="validation-error" v-if="errors.value">
                 {{ errors.value }}
-              </div>
-              <p class="color-text">
-                {{ _t('Quantity') }}
-              </p>
-              <input
-                type="text"
-                class="open-item quantity"
-                v-model.number="vitem.quantity"
-                placeholder="Item Quantity"
-                autocomplete="off"
-              />
-              <span class="qty-measurement-unit">
-                {{ item.measurement_unit }}</span
-              >
-              <div class="validation-error" v-if="errors.name">
-                {{ errors.name }}
               </div>
             </div>
           </form>
@@ -43,19 +40,19 @@
         <div class="modal-footer">
           <div class="btn-announce">
             <button
-              type="button"
-              class="btn btn-danger cancel-announce color-button"
-              data-dismiss="modal"
+                    type="button"
+                    class="btn btn-danger cancel-announce color-button"
+                    data-dismiss="modal"
             >
               {{ _t('Cancel') }}
             </button>
             <button
-              class="btn btn-success btn-large color-main "
-              type="button"
-              id="save-note"
-              @click="addToCart"
+                    class="btn btn-success btn-large color-main "
+                    type="button"
+                    id="save-note"
+                    @click="addToCart"
             >
-              {{ _t('Add') }}
+              {{ _t('Save') }}
             </button>
           </div>
         </div>
@@ -63,64 +60,62 @@
     </div>
   </div>
 </template>
-
+​
 <script>
-/* global hideModal */
-import { mapGetters, mapState } from 'vuex'
-export default {
-  data() {
-    return {
-      vitem: {
-        quantity: '',
-        value: '',
-      },
-      errors: {},
-    }
-  },
-  computed: {
-    ...mapState('location', ['currency']),
-    ...mapState('category', ['item']),
-    ...mapGetters('location', ['_t']),
-  },
-  methods: {
-    addToCart() {
-      this.errors = {}
-
-      if (!this.vitem.quantity) {
-        this.errors.quantity = this._t('Please provide item quantity')
+  /* global hideModal */
+  import { mapGetters, mapState } from 'vuex'
+  export default {
+    data() {
+      return {
+        item: {
+          name: '',
+          value: '',
+        },
+        errors: {},
       }
-
-      if (this.vitem.value < 0) {
+    },
+    computed: {
+      ...mapState('location', ['currency']),
+      ...mapGetters('location', ['_t']),
+    },
+    methods: {
+      addToCart() {
+        this.errors = {}
+​
+      if (!this.item.name) {
+        this.errors.name = this._t('Please provide item name')
+      }
+​
+      if (!this.item.value) {
         this.errors.value = this._t('Please provide item price')
       }
-
+​
       if (Object.keys(this.errors).length !== 0) {
         return false
       }
-
+​
       this.$store
-        .dispatch('order/addOpenItem', {
-          item: {},
-          data: { ...this.item, type: 'genericOpenItem' },
-        })
-        .then(() => {
-          hideModal('#open-item')
-          this.item = {}
-        })
+              .dispatch('order/addOpenItem', {
+                item: {},
+                data: { ...this.item, type: 'genericOpenItem' },
+              })
+              .then(() => {
+                hideModal('#open-item')
+                this.item = {}
+              })
+      },
     },
-  },
-}
+  }
 </script>
 <style lang="sass" scoped>
-.open-item
-  width: 85%
-  height: 3.125rem
-  border-radius: 3px
-  background-color: #ffffff
-  border: solid 1px #e4e7eb
-  padding: 0.625rem
-
-.validation-error
-  position: initial
-  font-size: 0.95rem
+  .open-item
+    width: 100%
+    height: 3.125rem
+    border-radius: 3px
+    background-color: #ffffff
+    border: solid 1px #e4e7eb
+    padding: 0.625rem
+  .validation-error
+    position: initial
+    font-size: 0.95rem
 </style>
