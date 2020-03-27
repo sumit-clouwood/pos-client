1ebc461ce79724e8b01bfe84767712154b690762
Abdul Jalil Ashraf <abdul.ashraf.jalil@gmail.com>
2020-03-27 11:10:12 +0500

    swap buttons and adjust styling on carhop screens

--- src/components/carhop/content/CompletedOrders.vue
+++ src/components/carhop/content/CompletedOrders.vue
@@ -163,10 +163,16 @@ export default {
     color: #7ac241
 </style>
 <style lang="scss" scoped>
-@import '../../../assets/scss/responsive_table.scss';
-@import '../../../assets/scss/pixels_rem.scss';
-@import '../../../assets/scss/variables.scss';
-@import '../../../assets/scss/mixins.scss';
+@import '@/assets/scss/responsive_table.scss';
+@import '@/assets/scss/pixels_rem.scss';
+@import '@/assets/scss/variables.scss';
+@import '@/assets/scss/mixins.scss';
+.carhop-completed-orders {
+  &::-webkit-scrollbar {
+    width: 0rem;
+    height: 0rem;
+  }
+}
 @include responsive(mobile) {
   .carhop-completed-orders-wrapper {
     padding: 10px;
