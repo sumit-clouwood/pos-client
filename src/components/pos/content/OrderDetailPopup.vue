990278a868def23d37289fba530ae70e9a81398d
Abdul Jalil Ashraf <abdul.ashraf.jalil@gmail.com>
2020-04-04 12:13:32 +0500

    fix ui issues for customer details and past orders popups

--- src/components/pos/content/OrderDetailPopup.vue
+++ src/components/pos/content/OrderDetailPopup.vue
@@ -115,7 +115,7 @@
                 class="button-caption"
                 data-toggle="modal"
                 data-target="#display-order"
-                @click="fetchSelectedCustomer(selectedOrder.customer._id)"
+                @click="fetchCustomer(selectedOrder.customer._id)"
               >
                 {{ _t('Open Past Orders') }}
               </div>
@@ -142,6 +142,7 @@
 </template>
 
 <script>
+/* global hideModal */
 import { mapState, mapGetters, mapActions } from 'vuex'
 
 import Invoice from '@/components/pos/content/cart/payNow/Invoice'
@@ -179,6 +180,10 @@ export default {
   methods: {
     ...mapActions('customer', ['fetchSelectedCustomer']),
     ...mapActions('deliveryManager', ['printInvoice']),
+    fetchCustomer(customerId) {
+      hideModal('#orderDetailsPopup')
+      this.fetchSelectedCustomer(customerId)
+    },
     printInvoiceDisableKitchenPrint(details) {
       let dt = this.$store.state.auth.deviceType
       let isIOS = dt.osType
