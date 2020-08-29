75352c8319904317643ec567b0adf922aa411bf6
sunarcdell <yuvraj.singh@sunarctechnologies.com>
2020-08-30 00:05:32 +0530

    5. Added map and its coordinate on Create new address & edit address --done 6. Worked on Edit customers

--- src/services/service-worker.js
+++ src/services/service-worker.js
@@ -3,7 +3,7 @@
 /* eslint-disable no-console */
 //appVersion has production build number . staging build number . int build number . bugfix
 //Reason: fixed msg 13
-var appVersion = '7.9.34.24'
+var appVersion = '7.9.34.25'
 
 var clientUrl = ''
 
