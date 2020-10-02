54d024427b5920d9d562a657cc2b08967375c752
sunarcdell <yuvraj.singh@sunarctechnologies.com>
2020-10-02 19:14:06 +0530

    Fixed Carhop API section

--- src/const/permissions.js
+++ src/const/permissions.js
@@ -14,3 +14,4 @@ export const SEE_OTHERS_ORDERS = 'can_see_others_orders'
 
 export const TOKEN_NUMBER = 'stores.o.store_token_screen_form_action_id'
 export const PRINT_BUSINESS_SUMMARY = 'orders.dashboard_business_summary'
+export const CARHOP_COMPLETED_ORDERS = 'orders.carhop_completed_orders.api'
