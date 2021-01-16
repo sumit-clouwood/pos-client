f08bf68171bb5a717bbae066b74cbb806682e561
Tofeeq <tof33q@gmail.com>
2021-01-16 12:40:22 +0500

    fix

--- src/store/modules/checkout.js
+++ src/store/modules/checkout.js
@@ -895,14 +895,7 @@ const actions = {
                         delete item.originalItem
                         return item
                       })
-                      //temp fix
-                      if (order.total_paid != order.balance_due) {
-                        let diff = Num.round(order.balance_due - order.total_paid)
-                        order.order_payments[0].collected = parseFloat(order.order_payments[0].collected) + diff
-                        order.order_payments[0].param2 = order.order_payments[0].collected
-                        order.total_paid = order.balance_due
-
-                      }
+                      
                       commit(mutation.SET_ORDER, order)
                       
                       dispatch('createOrder', { action: action, data: data })
