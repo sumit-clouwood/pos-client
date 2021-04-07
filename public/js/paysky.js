4cb3c6c38471a082b73ddc61ffab1cc839683815
Tofeeq <tof33q@gmail.com>
2021-04-07 11:12:48 +0500

    show the move card fast error

--- public/js/paysky.js
+++ public/js/paysky.js
@@ -9,7 +9,7 @@ function paySkyCallbackAndroid(functionName, data) {
   }
 }
 const simulatePaySky = localStorage.getItem('simulatePaySky')
-if (simulatePaySky) {
+if (simulatePaySky !== false) {
   // eslint-disable-next-line no-unused-vars
   var AndroidPOS = {
     callFunction: function(func, data, cb) {
@@ -43,14 +43,30 @@ if (simulatePaySky) {
       }
 
       Eventer.emit('paysky', jsonData, 'checkout')
-      setTimeout(() => {
-        jsonData.state = 'processing'
-        Eventer.emit('paysky', jsonData, 'checkout')
-        setTimeout(() => {
-          jsonData.state = 'success'
-          Eventer.emit('paysky', jsonData, 'checkout')
-        }, 3000)
-      }, 3000)
+
+      //simulatePaySky = [{"data":{"status":true,"state":"error","error_type":"move_card_fast"},"timeout":2000},{"data":{"status":true,"state":"success"},"timeout":3000}]
+
+      const events = JSON.parse(simulatePaySky)
+
+      events
+        .reduce(function(mypromise, event) {
+          return mypromise.then(() => {
+            event.data = Object.assign({}, jsonData, event.data)
+            return execChainProcess(event)
+          })
+        }, Promise.resolve())
+        .then(function(results) {
+          // all done here with array of results
+          console.log(results)
+        })
     },
   }
+}
+function execChainProcess(event) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      Eventer.emit('paysky', event.data, 'checkout')
+      resolve()
+    }, event.timeout)
+  })
 }
