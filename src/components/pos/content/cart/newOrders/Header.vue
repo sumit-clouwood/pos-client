216cebdffe5070fdd4d5937a2f9f4522fa02ff5c
sunarcdell <yuvraj.singh@sunarctechnologies.com>
2021-05-25 19:49:48 +0530

    worked on take away future order

--- src/components/pos/content/cart/newOrders/Header.vue
+++ src/components/pos/content/cart/newOrders/Header.vue
@@ -58,6 +58,51 @@
           <i aria-hidden="true" class="fa fa-chevron-down"></i>
         </div>
       </div>
+      <div
+        v-if="
+          orderType.OTApi === CONST.ORDER_TYPE_TAKEAWAY &&
+            enabled(CONST.MODULE_FUTURE_ORDERS)
+        "
+      >
+        <form>
+          <datetime
+            type="datetime"
+            title="Schedule order"
+            placeholder="Schedule"
+            v-model="getSetDate"
+            input-class="btn schedule-input btn-large datepicker-here color-dashboard-background"
+            :value-zone="timezoneString"
+            :zone="timezoneString"
+            :format="{
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+              hour: 'numeric',
+              minute: '2-digit',
+            }"
+            :phrases="{ ok: 'Continue', cancel: 'No' }"
+            :hour-step="1"
+            :minute-step="10"
+            :min-datetime="minDatetime"
+            :max-datetime="maxDatetime"
+            :week-start="7"
+            :hide-backdrop="false"
+            use12-hour
+            auto
+          >
+            <!--<label for="startDate" slot="before">Field Label</label>-->
+            <!--<span class="description" slot="after">The field description</span>-->
+            <template slot="button-cancel">
+              <!--<fa :icon="['far', 'times']"></fa>-->
+              <span @click="setFutureDate(false)">{{ _t('No') }}</span>
+            </template>
+            <template slot="button-confirm">
+              <!--<fa :icon="['fas', 'check-circle']"></fa>-->
+              <span @click="setFutureDate(true)">{{ _t('Continue') }}</span>
+            </template>
+          </datetime>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -68,6 +113,7 @@ import CheckoutMixin from '@/mixins/Checkout'
 
 import { mapState, mapGetters, mapActions } from 'vuex'
 import DateTimeVue from '@/components/util/DateTimeVue'
+import moment from 'moment-timezone'
 export default {
   name: 'Header',
   mixins: [CheckoutMixin],
@@ -76,6 +122,9 @@ export default {
     return {
       showScrollUp: false,
       showScrollDown: false,
+      futureDateTime: '',
+      minDatetime: new Date().toISOString(),
+      maxDatetime: null,
     }
   },
   components: {
@@ -83,11 +132,28 @@ export default {
   },
   computed: {
     ...mapGetters('location', ['_t']),
-    ...mapState('order', ['items', 'cartType', 'orderType']),
+    ...mapState('order', ['items', 'cartType', 'orderType', 'futureOrder']),
     ...mapState('checkoutForm', ['msg']),
+    ...mapState('location', ['timezoneString']),
     ...mapState({ selectedCustomer: state => state.customer.customer }),
     ...mapState({ offlineCustomer: state => state.customer.offlineData }),
     ...mapState('sync', ['online']),
+    ...mapGetters('modules', ['enabled']),
+    getSetDate: {
+      get() {
+        return this.futureDateTime
+      },
+      set(datetime) {
+        if (datetime !== '' && typeof datetime.type === 'undefined') {
+          this.futureDateTime = datetime
+          let date_time =
+            datetime != ''
+              ? moment.utc(datetime).format('YYYY/MM/DD HH:mm')
+              : null
+          if (date_time) this.$store.commit('order/SET_FUTURE_ORDER', date_time)
+        }
+      },
+    },
   },
   mounted() {
     bus.$on('showScrollCartUp', option => {
@@ -97,7 +163,16 @@ export default {
       this.showScrollDown = option
     })
   },
+  updated() {
+    if (!this.futureOrder) this.futureDateTime = ''
+  },
   methods: {
+    setFutureDate(status) {
+      if (!status) {
+        this.futureDateTime = ''
+        this.$store.commit('order/SET_FUTURE_ORDER', false)
+      }
+    },
     scroll(option) {
       bus.$emit('scroll-cart', option)
     },
