1c684630ec5447baba27f320ba7d2ece0c16d3f8
Yuvraj Singh <yuvraj.singh@sunarctechnologies.com>
2021-08-17 19:18:11 +0530

    Print action for carhop and dine in { Running & Completed order}

--- src/components/carhop/content/RunningOrders.vue
+++ src/components/carhop/content/RunningOrders.vue
@@ -86,52 +86,7 @@
                     </button>
                   </div>
 
-                  <div class="dropdown">
-                    <button
-                      class="button btn btn-success color-main color-text-invert dropdown-toggle"
-                      type="button"
-                      id="dropdownMenuButton"
-                      @click="selectedOrderDetails(order._id)"
-                      data-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="22"
-                        height="19"
-                        viewBox="0 0 22 19"
-                      >
-                        <g fill="#fff" fill-rule="nonzero">
-                          <path
-                            d="M2.68 7.096c0-2.825-.274-2.568 1.51-2.568V1.113c0-.53.438-.963.968-.963h11.684c.53 0 .967.433.967.963v3.415c1.785 0 1.511-.257 1.511 2.568.832 0 2.44-.26 2.44.95v5.993a.96.96 0 0 1-.963.946h-2.988v2.832a.971.971 0 0 1-.967.963H5.158a.971.971 0 0 1-.967-.963v-2.832H1.203a.96.96 0 0 1-.964-.946V8.046c0-.527.44-.95.964-.95H2.68zM3.702 5.55v1.546h.489V5.55h-.489zm14.596 0h-.489v1.546h.489V5.55zm-.489 7.425v.987h2.93V8.12H1.261v5.843H4.19c0-.499-.177-1.951.967-1.951h11.684c.593 0 .964.596.964.964h.003zm-1.022 4.78l-.007-4.718H5.217l-.004 4.717h11.574zM5.213 1.171v5.924h11.574V1.172H5.213z"
-                          />
-                          <path
-                            d="M17.518 9.8c-.672 0-.672-1.023 0-1.023h1.643c.676 0 .676 1.023 0 1.023h-1.643z"
-                          />
-                        </g>
-                      </svg>
-                      <span>{{ _t('Print') }}</span>
-                    </button>
-                    <div
-                      class="dropdown-menu dropdown-menu-right animate slideIn"
-                      aria-labelledby="dropdownMenuButton"
-                    >
-                      <a
-                        class="dropdown-item"
-                        role="button"
-                        v-for="(template, index) in selectedOrder.invoice"
-                        :key="index"
-                        @click="
-                          printInvoice({
-                            templateId: template._id,
-                            order: selectedOrder,
-                          })
-                        "
-                        >{{ template.name }}</a
-                      >
-                    </div>
-                  </div>
+                  <print-action :orderId="order._id"></print-action>
                   <router-link
                     v-if="canPay && !order.order_payments.length"
                     :to="'/carhop' + store + '/' + order._id"
@@ -227,6 +182,7 @@
 import { mapActions, mapState, mapGetters } from 'vuex'
 import paginate from 'vuejs-paginate'
 import ReadyAction from './ReadyAction'
+import PrintAction from '@/components/util/Action/PrintAction.vue'
 
 export default {
   name: 'CompletedOrders',
@@ -236,6 +192,7 @@ export default {
   components: {
     ReadyAction,
     paginate,
+    PrintAction,
   },
   data() {
     return {
@@ -270,7 +227,6 @@ export default {
   methods: {
     ...mapActions('carhop', ['fetchOrders', 'orderCollected']),
     ...mapActions('order', ['selectedOrderDetails']),
-    ...mapActions('deliveryManager', ['printInvoice']),
     orderCollection(orderId) {
       this.processedOrder.push(orderId)
       this.orderCollected(orderId).then(() => {
--- src/components/dinein/content/OrderList.vue
+++ src/components/dinein/content/OrderList.vue
@@ -99,53 +99,7 @@
                             order.order_system_status !== 'requires_acceptance'
                           "
                         >
-                          <div class="dropdown">
-                            <button
-                              class="button btn btn-success color-main color-text-invert dropdown-toggle"
-                              type="button"
-                              id="dropdownMenuButton"
-                              @click="selectedOrderDetails(order._id)"
-                              data-toggle="dropdown"
-                              aria-haspopup="true"
-                              aria-expanded="false"
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="22"
-                                height="19"
-                                viewBox="0 0 22 19"
-                              >
-                                <g fill="#fff" fill-rule="nonzero">
-                                  <path
-                                    d="M2.68 7.096c0-2.825-.274-2.568 1.51-2.568V1.113c0-.53.438-.963.968-.963h11.684c.53 0 .967.433.967.963v3.415c1.785 0 1.511-.257 1.511 2.568.832 0 2.44-.26 2.44.95v5.993a.96.96 0 0 1-.963.946h-2.988v2.832a.971.971 0 0 1-.967.963H5.158a.971.971 0 0 1-.967-.963v-2.832H1.203a.96.96 0 0 1-.964-.946V8.046c0-.527.44-.95.964-.95H2.68zM3.702 5.55v1.546h.489V5.55h-.489zm14.596 0h-.489v1.546h.489V5.55zm-.489 7.425v.987h2.93V8.12H1.261v5.843H4.19c0-.499-.177-1.951.967-1.951h11.684c.593 0 .964.596.964.964h.003zm-1.022 4.78l-.007-4.718H5.217l-.004 4.717h11.574zM5.213 1.171v5.924h11.574V1.172H5.213z"
-                                  />
-                                  <path
-                                    d="M17.518 9.8c-.672 0-.672-1.023 0-1.023h1.643c.676 0 .676 1.023 0 1.023h-1.643z"
-                                  />
-                                </g>
-                              </svg>
-                              {{ _t('Print') }}
-                            </button>
-                            <div
-                              class="dropdown-menu"
-                              aria-labelledby="dropdownMenuButton"
-                            >
-                              <a
-                                class="dropdown-item"
-                                role="button"
-                                v-for="(template,
-                                index) in selectedOrder.invoice"
-                                :key="index"
-                                @click="
-                                  printInvoice({
-                                    templateId: template._id,
-                                    order: selectedOrder,
-                                  })
-                                "
-                                >{{ template.name }}</a
-                              >
-                            </div>
-                          </div>
+                          <print-action :orderId="order._id"></print-action>
                           <button
                             class="dinefor-paynow btn btn-success"
                             v-if="
@@ -331,6 +285,7 @@ import Preloader from '@/components/util/Preloader'
 import paginate from 'vuejs-paginate'
 import InformationPopup from '@/components/pos/content/InformationPopup'
 import OrderAcceptReject from './buttons/OrderAcceptReject'
+import PrintAction from '@/components/util/Action/PrintAction'
 
 export default {
   name: 'OrderList',
@@ -343,6 +298,7 @@ export default {
     paginate,
     InformationPopup,
     OrderAcceptReject,
+    PrintAction,
   },
   data() {
     return {
@@ -446,7 +402,6 @@ export default {
       'fetchOrderDetailsOnly',
       'updateOrderAction',
     ]),
-    ...mapActions('deliveryManager', ['printInvoice']),
     ...mapActions('dinein', [
       'reservationUpdateStatus',
       'fetchMoreReservations',
