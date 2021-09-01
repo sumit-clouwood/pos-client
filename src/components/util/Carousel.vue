5c98010a71489103afa4c535765295f476500142
Yuvraj Singh <yuvraj.singh@sunarctechnologies.com>
2021-09-01 17:47:11 +0530

    added an handle in case no group selected

--- src/components/util/Carousel.vue
+++ src/components/util/Carousel.vue
@@ -229,9 +229,12 @@ export default {
       }
     },
     fetchImage(method) {
-      return this.$store.getters['payment/findAggregateGroup'](
-        method.payment_type_group
-      )['icon']
+      if (method.payment_type_group) {
+        return this.$store.getters['payment/findAggregateGroup'](
+          method.payment_type_group
+        )['icon']
+      }
+      return ''
     },
     movePage(page) {
       if (this.totalPages === 1) return
