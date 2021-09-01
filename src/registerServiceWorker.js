e30eb9bc76dfe79f0202180799c96e314b4df0ec
Tofeeq <tof33q@gmail.com>
2021-09-01 17:08:46 +0500

    fix time out

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -12,9 +12,7 @@ const notifyUserAboutUpdate = worker => {
   // store.commit('sync/setAppUpdateNotification', true)
   // localStorage.setItem('update_available', true)
   let lastUpdatedTime = localStorage.getItem('pos_version_updated_on') || 0
-  lastUpdatedTime =
-    parseInt(lastUpdatedTime) +
-    1000 * 60 * process.env.VUE_APP_SYSTEM_UPDATE_TIMEOUT
+  lastUpdatedTime = parseInt(lastUpdatedTime) + 1000 * 60 * 5
 
   console.log(
     1,
