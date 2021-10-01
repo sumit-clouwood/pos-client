a9f6d6348c22e2b7008738d8b9fad2a1c6666fc0
Tofeeq <tof33q@gmail.com>
2021-10-01 14:56:08 +0500

    refferals revamp

--- src/components/pos/footer/popups/Referrals.vue
+++ src/components/pos/footer/popups/Referrals.vue
@@ -18,7 +18,7 @@
           <i class="fa fa-chevron-down" aria-hidden="true"></i>
         </div>
 
-        <div class="modal-body row dining-options-block select-discount">
+        <div class="modal-body row">
           <!--<p v-if="errors !== ''" class="errors text-danger">{{ errors }}</p>-->
           <!--<p v-if="msg" class="text-info">{{ msg }}</p>-->
           <div class="error mx-auto" v-show="!referrals">
@@ -26,37 +26,35 @@
               <span> {{ _t('Referrals are not available.') }}</span>
             </p>
           </div>
-          <div
-            v-show="referrals"
-            class="dining-option-block select-discount-option"
-            ref="referralsContainer"
-          >
-            <div
-              class="option-contain"
-              ref="entityReferral"
-              :class="{
-                active: referral._id === changedReferral.referralId,
-                'color-dashboard-background': true,
-              }"
-              v-for="referral in referrals"
-              :key="referral._id"
-              @click="
-                selectedReferral({
-                  referralName: referral.name,
-                  referralId: referral._id,
-                })
-              "
-            >
-              <p
-                class="color-text-invert text-uppercase"
-                v-if="referral.referral_type === 'cod'"
+          <div class="refferals-wrapper">
+            <div v-show="referrals" class="referrals" ref="referralsContainer">
+              <div
+                class="option-contain"
+                ref="entityReferral"
+                :class="{
+                  active: referral._id === changedReferral.referralId,
+                  'color-dashboard-background': true,
+                }"
+                v-for="referral in referrals"
+                :key="referral._id"
+                @click="
+                  selectedReferral({
+                    referralName: referral.name,
+                    referralId: referral._id,
+                  })
+                "
               >
-                {{ referral.referral_type }}
-              </p>
-              <p class="color-text-invert text-capitalize" v-else>
-                {{ referral.referral_type }}
-              </p>
-              <h6 class="more color-text">{{ referral.name }}</h6>
+                <p
+                  class="reftitle color-text-invert text-uppercase"
+                  v-if="referral.referral_type === 'cod'"
+                >
+                  {{ referral.referral_type }}
+                </p>
+                <p class="reftitle color-text-invert text-capitalize" v-else>
+                  {{ referral.referral_type }}
+                </p>
+                <h6 class="more color-text">{{ referral.name }}</h6>
+              </div>
             </div>
           </div>
         </div>
@@ -99,7 +97,7 @@ export default {
       this.$nextTick(() => {
         setTimeout(() => {
           this.calculateScrolls().catch(() => {})
-        }, 500)
+        }, 300)
       })
     })
   },
@@ -128,11 +126,39 @@ export default {
 }
 </script>
 <style lang="sass" scoped>
+.refferals-wrapper
+  max-height: 20rem;
+  overflow: hidden;
+  width: 100%;
+  scroll-behavior: smooth;
+
+  .referrals
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 1.25rem;
+
+    .option-contain
+      cursor: pointer;
+      text-align: left;
+      padding: 0.6rem;
+
+      .reftitle
+        border-radius: 3px;
+        background-color: #f99c32;
+        display: inline-block;
+        padding: 0.1875rem 0.5rem;
+        margin-bottom: 0.3125rem;
+        color: #ffffff;
+        font-size: 0.875rem;
+        font-weight: 700;
+        letter-spacing: 0.5px;
+
 .error
   width: 100%
 
 #select-referral
   z-index: 9999
+
 
 .modal-dialog
     max-width: 40%
@@ -141,13 +167,10 @@ export default {
   right: 10px;
 
 .referral-bottom-arrow
-  top: 53px;
+  top: 58px;
 
 .refferal-top-arrow
   &.food-arrow
     right: 10px;
-    bottom: 64px;
-
-.select-discount
-  scroll-behavior: smooth
+    bottom: 84px;
 </style>
