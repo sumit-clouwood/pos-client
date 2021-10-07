49e15c12e984aa9447972c173dbc3d1d3063e138
Tofeeq <tof33q@gmail.com>
2021-10-07 16:34:56 +0500

    fix for showing supervisor error

--- src/store/modules/checkout.js
+++ src/store/modules/checkout.js
@@ -2124,7 +2124,7 @@ const actions = {
       return dispatch('setMessage', {
         result: 'error',
         msg: error.message || error,
-        desc: error.response.data
+        desc: (error.response && error.response.data) ? error.response.data : error
       })
     }
   },
